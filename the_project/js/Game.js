ff92c30ea0423fc3ecb85931d0031a095a1ba0f7
Nick Huemmer <njhuemmer@gmail.com>
2021-03-10 17:23:40 -0800

    fixed the color problem with the token - player 1 is now red (it was black before)

--- the_project/js/Game.js
+++ the_project/js/Game.js
@@ -10,8 +10,8 @@ class Game {
  * @return {array}  An array of two Player objects.
  */
     createPlayers() {
-        const players = [new Player('Player 1', '#e15258', 1, true),
-                        new Player('Player 2', '#e59a13', 2)]
+        const players = [new Player('Player 1',  1, '#e15258', true),
+                        new Player('Player 2',  2, '#e59a13',)]
         return players;
     }
 
